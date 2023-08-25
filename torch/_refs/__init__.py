e298d5351064c0ad1464b92ac10d5f85a0c9cfca
Digant Desai <digantdesai@meta.com>
2023-08-25 09:22:04 -0700

    [PyTorch] torch.empty_permuted: rename param name from 'physical_layout' to 'dim_order'
    Summary:
    Alignes well with the new `tensor.dim_order()` fn.
    Since this is arg and not kwarg, and same dtype, this change shouldn't be BC breaking.
    
    Test Plan: CI
    
    Differential Revision: D48176693
    
    fbshipit-source-id: c41927b4887b2f34a92bef79d9891d95ab093105

--- torch/_refs/__init__.py
+++ torch/_refs/__init__.py
@@ -4450,7 +4450,7 @@ def empty(
 @out_wrapper()
 def empty_permuted(
     shape,
-    physical_layout,
+    dim_order,
     dtype: Optional[torch.dtype] = None,
     layout: torch.layout = torch.strided,
     device: Optional[torch.device] = None,
@@ -4459,7 +4459,7 @@ def empty_permuted(
 ) -> TensorLikeType:
     return prims.empty_permuted(
         shape,
-        physical_layout,
+        dim_order,
         dtype=dtype,
         device=device,
         requires_grad=requires_grad,
