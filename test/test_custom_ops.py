2507041e87d746aeb1d45ca54664d13be7a3f94f
Yanbo Liang <ybliang8@gmail.com>
2023-09-06 17:15:09 -0700

    Update

--- test/test_custom_ops.py
+++ test/test_custom_ops.py
@@ -1491,9 +1491,8 @@ def forward(self, x_1):
         from torch._dynamo.utils import counters
 
         counters.clear()
-        cnt = torch._dynamo.testing.CompileCounter()
 
-        @torch.compile(backend=cnt)
+        @torch.compile(backend="inductor")
         def f(x):
             return torch.ops._torch_testing.numpy_nonzero(x.clone()).clone()
 
