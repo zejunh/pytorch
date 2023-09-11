cb7e9d98dee6047338e50971295ec259d8e5cbbc
Elias Ellison <elias.ellison@gmail.com>
2023-09-11 22:33:17 +0000

    serialize more patterns
    ghstack-source-id: 479d00de3befff95f068cd8ff0d6cb59f6c09487
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/108918

--- torch/_inductor/fx_passes/serialized_patterns/central_index.py
+++ torch/_inductor/fx_passes/serialized_patterns/central_index.py
@@ -7,6 +7,10 @@ from ._sfdp_pattern_2 import (_sfdp_pattern_2_training, _sfdp_pattern_2_inferenc
 from ._sfdp_pattern_3 import (_sfdp_pattern_3_training, _sfdp_pattern_3_inference)
 from ._sfdp_pattern_4 import (_sfdp_pattern_4_training, _sfdp_pattern_4_inference)
 from ._sfdp_pattern_5 import (_sfdp_pattern_5_training, _sfdp_pattern_5_inference)
+from ._sfdp_pattern_6 import (_sfdp_pattern_6_training, _sfdp_pattern_6_inference)
+from ._sfdp_pattern_7 import (_sfdp_pattern_7_training, _sfdp_pattern_7_inference)
+from ._sfdp_pattern_8 import (_sfdp_pattern_8_training, _sfdp_pattern_8_inference)
+from ._sfdp_pattern_9 import (_sfdp_pattern_9_training, _sfdp_pattern_9_inference)
 
 central_index = {
     '_sfdp_pattern_1_training': _sfdp_pattern_1_training,
@@ -19,6 +23,14 @@ central_index = {
     '_sfdp_pattern_4_inference': _sfdp_pattern_4_inference,
     '_sfdp_pattern_5_training': _sfdp_pattern_5_training,
     '_sfdp_pattern_5_inference': _sfdp_pattern_5_inference,
+    '_sfdp_pattern_6_training': _sfdp_pattern_6_training,
+    '_sfdp_pattern_6_inference': _sfdp_pattern_6_inference,
+    '_sfdp_pattern_7_training': _sfdp_pattern_7_training,
+    '_sfdp_pattern_7_inference': _sfdp_pattern_7_inference,
+    '_sfdp_pattern_8_training': _sfdp_pattern_8_training,
+    '_sfdp_pattern_8_inference': _sfdp_pattern_8_inference,
+    '_sfdp_pattern_9_training': _sfdp_pattern_9_training,
+    '_sfdp_pattern_9_inference': _sfdp_pattern_9_inference,
 }
 
 
--- torchgen/fuse_attention_patterns/gen_attention_patterns.py
+++ torchgen/fuse_attention_patterns/gen_attention_patterns.py
@@ -97,7 +97,7 @@ def serialize_functions() -> None:
         }
 
         # temporary to batch adding new patterns
-        if i >= 10:
+        if i >= 18:
             continue
 
         from torch._functorch import config as functorch_config
