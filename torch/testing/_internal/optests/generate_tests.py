23bb5fd74401840f7003c70cf3b3e1bb8358d08b
Edward Z. Yang <ezyang@meta.com>
2023-09-12 21:41:13 -0400

    optests improvements based on torchvision usage on nms
    - Update cross-ref FakeMode test to use ShapeEnv.  Dynamic ops can now
      return an unbacked SymInt.  We always accept this as equal to whatever
      the real value was.
    - Relax test so it works on all classes, not just unittest.TestCase
    - Properly wrap the original method, so things like
      pytree.mark.parametrize are carried over
    
    Fixes https://github.com/pytorch/pytorch/issues/108927
    
    Signed-off-by: Edward Z. Yang <ezyangmeta.com>
    
    ghstack-source-id: f43afb90c22da35050562738fc6c93fe650c3fd7
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/108929

--- torch/testing/_internal/optests/generate_tests.py
+++ torch/testing/_internal/optests/generate_tests.py
@@ -2,7 +2,6 @@ import datetime
 import functools
 import os
 import tempfile
-import unittest
 
 import torch
 
@@ -38,7 +37,7 @@ def safe_autograd_registration_check(op, args, kwargs):
 
 def safe_fake_check(op, args, kwargs):
     args, kwargs = deepcopy_tensors((args, kwargs))
-    return fake_check(op, args, kwargs, dynamic_only=False)
+    return fake_check(op, args, kwargs)
 
 
 def safe_aot_autograd_check(op, args, kwargs, dynamic):
@@ -119,10 +118,6 @@ def generate_opcheck_tests(
         additional_decorators: Pass us some decorators
         test_utils: a list of test_utils to generate. Example: ["test_schema", "test_faketensor"]
     """
-    if not issubclass(testcase, unittest.TestCase):
-        raise ValueError(
-            f"Expected testcase to be subclass of unittest.TestCase, got {type(testcase)}"
-        )
     test_methods = [
         m
         for m in dir(testcase)
@@ -134,6 +129,7 @@ def generate_opcheck_tests(
         method = getattr(testcase, attr)
         new_method_name = prefix + "__" + attr
 
+        @functools.wraps(method)
         def new_method(*args, **kwargs):
             with OpCheckMode(
                 namespaces,
